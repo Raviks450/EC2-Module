58e1b6def787b583490020fb4eb938122c6cd438
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-11 18:59:48 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,17 +1,18 @@
 resource "google_compute_instance" "default" {
   name         = MyTerraformVM
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  machine_type = var.machinetype
+  region       = var.region
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.image
     }
   }
   
   # Define the network interface using the "default" network
   network_interface {
-    network = "default"
+    network = var.network
     # Assign an ephemeral public IP
     access_config {
     }
