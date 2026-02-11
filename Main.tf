4a484f9b02341ddd19e8d5f26045cad7d5770dab
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-11 18:20:49 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "default" {
+  name         = MyTerraformVM
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  
+  # Define the network interface using the "default" network
+  network_interface {
+    network = "default"
+    # Assign an ephemeral public IP
+    access_config {
+    }
+  }
+}
