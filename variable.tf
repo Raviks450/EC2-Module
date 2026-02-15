14dc8e4c348e82779df15770901febd352f836c3
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 22:41:02 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "project" {
+default = "qwiklabs-gcp-02-b47e52381718"
+}
+
+variable "region" {
+    default =    "us-central1"
+}
+
+variable "zone"{
+    default = "us-central1-c"
+}
+variable "machine_type"{
+    default = "n1-standard-1"
+}
+variable "name"{
+    default = "myterraform-from-module"
+}
+variable "image" {
+    default = "projects/debian-cloud/global/images/debian-12-bookworm-v20260210"
+}
+
+variable "network" {
+  type     = string
+  default = "default"
+}
