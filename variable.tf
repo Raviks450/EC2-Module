8cfbb8154460d7a26df8bccd3913f0ea32e6929d
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 17:08:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,25 +1,22 @@
-variable "region"{
-region      = "europe-west1"
+variable "project" {
+
+default = "qwiklabs-gcp-01-843b3377da7b"
+
+}
+
+variable "region" {
+    default =    "us-central1"
 }
 
 variable "zone"{
-zone        = "europe-west1-c"
+    default = "us-central1-c"
 }
-
+variable "machine_type"{
+    default = "n1-standard-1"
+}
 variable "name"{
-name        = "MyterraformVM"
+    default = "myterraform"
 }
-variable "machinetype"{
-machine_type = "projects/debian-cloud/global/images/debian-12-bookworm-v20260210"
+variable "image" {
+    default = "projects/debian-cloud/global/images/debian-12-bookworm-v20260210"
 }
-
-variable "image"{
-image       = "n1-standard-1"
-}
-
-variable "network"{
-network = "default"
-}
-
-
-
