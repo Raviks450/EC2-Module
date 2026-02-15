0c4ecb653496d9af8e0afae4b8154c71c3a7c4b5
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 22:12:52 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -14,7 +14,7 @@ resource "google_compute_instance" "default" {
   
 # Define the network interface using the "default" network
   network_interface {
-    network =var.network
+    name =var.network
     # Assign an ephemeral public IP
     access_config {
     }
