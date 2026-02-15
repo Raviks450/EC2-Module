86438f947940036504cf025be9602b27f6a1fdd4
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 17:23:02 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -4,6 +4,7 @@ resource "google_compute_instance" "default" {
   region       = var.region
   zone         = var.zone
   project      =var.project
+  network      = var.network
 
   boot_disk {
     initialize_params {
