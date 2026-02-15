6e1eeafa68238e311b7ceb445a379a2f30bd6bdd
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 20:43:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ default = "qwiklabs-gcp-02-1e9645190d28"
 }
 
 variable "network" {
-name = "default"
+name = "my-vpc"
 }
 
 
@@ -23,6 +23,7 @@ variable "name"{
 variable "image" {
     default = "projects/debian-cloud/global/images/debian-12-bookworm-v20260210"
 }
+
 
 
 
