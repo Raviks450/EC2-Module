8203c5738391f77c2195ff735025f4d4be1bccce
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 17:16:30 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,8 +1,3 @@
-module "repository" {
-  source  = "mineiros-io/repository/github"
-  version = "0.18.0"
-}
-
 resource "google_compute_instance" "default" {
   name         = var.name
   machine_type = var.machine_type
